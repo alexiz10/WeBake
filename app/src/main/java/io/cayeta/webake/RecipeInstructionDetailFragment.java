aede3f84f1ad4760a909e7bc651c2bf3aa89e710
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-07-16 02:50:21 -0700

    added instruction detail fragment basic structure

--- app/src/main/java/io/cayeta/webake/RecipeInstructionDetailFragment.java
+++ app/src/main/java/io/cayeta/webake/RecipeInstructionDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,14 +14,37 @@ import io.cayeta.webake.models.Instruction;
 
 public class RecipeInstructionDetailFragment extends Fragment {
 
+    private static final String ARGUMENT_INSTRUCTIONS_KEY = "instructions";
+    private static final String ARGUMENT_SELECTED_INSTRUCTION_POSITION = "selected_instruction";
+
+    private boolean mDualPane;
+
     public static RecipeInstructionDetailFragment newInstance(ArrayList<Instruction> instructions, int position) {
-        return new RecipeInstructionDetailFragment();
+        RecipeInstructionDetailFragment recipeInstructionDetailFragment = new RecipeInstructionDetailFragment();
+
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARGUMENT_INSTRUCTIONS_KEY, instructions);
+        args.putInt(ARGUMENT_SELECTED_INSTRUCTION_POSITION, position);
+        recipeInstructionDetailFragment.setArguments(args);
+
+        return recipeInstructionDetailFragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recipe_instruction_detail, container, false);
+        final View view = inflater.inflate(R.layout.fragment_recipe_instruction_detail, container, false);
+
+        mDualPane = getResources().getBoolean(R.bool.isTablet);
+
+        Bundle bundle = getArguments();
+
+        ArrayList<Instruction> instructions = bundle.getParcelableArrayList(ARGUMENT_INSTRUCTIONS_KEY);
+        int currentPosition = bundle.getInt(ARGUMENT_SELECTED_INSTRUCTION_POSITION);
+
+        // TODO: Set Details Here
+
+        return view;
     }
 
 }
