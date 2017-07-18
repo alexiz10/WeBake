8ab3001c33a67530b7849265c0dd2179c19d834d
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-07-17 22:34:30 -0700

    added code to hide navigation bar when video is viewed in fullscreen

--- app/src/main/java/io/cayeta/webake/RecipeDetailsActivity.java
+++ app/src/main/java/io/cayeta/webake/RecipeDetailsActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
+import android.view.View;
 
 import io.cayeta.webake.models.Recipe;
 
@@ -16,7 +16,10 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_recipe_details);
